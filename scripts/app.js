6e01a0beef9016e0455dffdede5ae6f4b95fef63
Aakash4792 <72347057+Aakash4792@users.noreply.github.com>
2021-09-23 15:10:56 +0530

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,57 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.display');
+const time = document.querySelector('.time');
+const icon = document.querySelector('.iconImg');
+
+const updateUI = (data) => {
+
+    const {cityDet,weatherDet} = data;
+
+    details.innerHTML = `
+        <h3>${cityDet.EnglishName}</h3>
+        <div class="t2">
+            ${weatherDet.WeatherText}
+        </div>
+        <div class="t3">
+            <span>${weatherDet.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    let iconSrc = `img/icons/${weatherDet.WeatherIcon}.svg`;
+    icon.setAttribute('src',iconSrc);
+
+    let timeSrc = weatherDet.IsDayTime?'img/dayfinal.jpg':'img/nightfinal.jpg';
+    time.setAttribute('src',timeSrc);
+
+    if(card.classList.contains('hide')){
+        card.classList.remove('hide');
+    }
+
+}
+const updateCity = async(cityName) => {
+
+    const cityDet = await getCityDet(cityName);
+    const weatherDet = await getWeatherDet(cityDet.Key);
+
+    return {cityDet,weatherDet};
+}
+
+
+
+cityForm.addEventListener('submit',e=>{
+    e.preventDefault();
+
+    const cityName = cityForm.city.value.trim();
+    cityForm.reset();
+
+    updateCity(cityName)
+        .then((data)=>{
+            updateUI(data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+})
+
